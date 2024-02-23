457102ff684083419303288cb0f32fdc37dde451
tellmebaby <tellmebaby407@gmail.com>
2024-02-23 18:24:55 +0900

    20240223 수업후

---
+++ 보강/코딩테스트기초1/src/스택/스택.java
@@ -0,0 +1,16 @@
+package 스택;
+
+import java.util.Stack;
+
+public class 스택 {
+
+	static Stack<Integer> stk;
+	
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+
+
---
+++ 보강/코딩테스트기초1/src/큐/큐.java
@@ -0,0 +1,8 @@
+package 큐;
+
+public class 큐 {
+
+	public static void main(String[] args) {
+		
+	}
+}
